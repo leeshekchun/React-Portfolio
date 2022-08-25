c51f01499f32365546f3022824bf5ac0a3b24c30
ShekChun Lee <leeshekchun@gmail.com>
2022-08-25 14:28:18 -0600

    add components files

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import coverImage from '';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+            My name is Chris Lee. I am currently working as an Automotive tech in Tesla.
+            I love to solve problems, teach one another, and learn new things.
+            Some of my hobbies are pickleball, chinese chess, and making food.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
