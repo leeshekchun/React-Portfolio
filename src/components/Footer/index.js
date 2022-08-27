334ea714c6e35cc013c315cf634d8229553f21e2
ShekChun Lee <leeshekchun@gmail.com>
2022-08-27 06:42:42 -1000

    add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { SiLinkedin, GoMarkGithub, HiOutlineMail} from "react-icons/bs";
 
 function Footer() {
+    return (
+        <footer>
+            <div>
+                <a href='https://www.linkedin.com/in/shekchunlee/'><h3><SiLinkedin/>LinkedIn</h3></a>
+            </div>
 
+            <div>
+                <a href='https://github.com/leeshekchun'><h3><GoMarkGithub/>Github</h3></a>
+            </div>
+
+            <div>
+                <a href='leeshekchun@gmail.com'><h3><HiOutlineMail/>Email</h3></a>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer;
