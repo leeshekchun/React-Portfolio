06190b47428a58da858b651e095ed1c8f8d9431d
ShekChun Lee <leeshekchun@gmail.com>
2022-08-29 08:31:47 -1000

    added projects

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,6 @@ import React from "react";
 function Header() {
   return (
     <header>
-        <div>
-            <img>
-            </img>
-        </div>
       <div>
         <h2>Hi, I'm Chris Lee!</h2>
         <h3>I'm a Full Stack Web Developer</h3>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React from "react";
 
 function Projects() {
+  return (
+    <section id="my-project">
+      <h1 class="section-title">My Project</h1>
 
+      <div class="run-buddy">
+        <a href="https://leeshekchun.github.io/run-buddy/index.html">
+          <img
+            src="./assets/images/hero-bg.jpg"
+            alt="run-buddy-project-cover"
+          />
+
+          <div class="run-buddy-text">
+            <h3>RUN-BUDDY</h3>
+          </div>
+        </a>
+      </div>
+
+      <div class="horiseon">
+        <a href="https://leeshekchun.github.io/Horiseon-refactor/">
+          <img
+            src="./assets/images/digital-marketing-meeting.jpg"
+            alt="digital-marketing-meeting"
+          />
+
+          <div class="horiseon-text">
+            <h3>HORISEON</h3>
+          </div>
+        </a>
+      </div>
+
+      <div class="KevinBacon">
+        <a href="https://dawnwogerman.github.io/6degreesofkevinbacon/">
+          <img src="./assets/images/kevinbacon-herobg.jpg" alt="Kevin Bacon" />
+
+          <div class="kevinBacon-text">
+            <h3>Six Degree of Kevin Bacon</h3>
+          </div>
+        </a>
+      </div>
+
+      <div class="petsagram">
+        <a href="https://petsagram-project2.herokuapp.com/">
+          <img src="./assets/images/petsagram.png" alt="petsagram-project2" />
+
+          <div class="petsagram-text">
+            <h3>Petsagram</h3>
+          </div>
+        </a>
+      </div>
+
+      <div class="artofJenna">
+        <a href="https://artofjenna.herokuapp.com/">
+          <img
+            src="./assets/images/art-of-jenna-louise.png"
+            alt="artOfJenna-project3"
+          />
+
+          <div class="artOfJenna-text">
+            <h3>Art-of-Jenna-Louise</h3>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
