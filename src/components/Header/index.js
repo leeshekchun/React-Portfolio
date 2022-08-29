abe9b5653c6824161e5d66da894f1a71fd1a701e
ShekChun Lee <leeshekchun@gmail.com>
2022-08-28 21:50:46 -1000

    modified nav componenets

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
-    
+  return (
+    <header>
+        <div>
+            <img>
+            </img>
+        </div>
+      <div>
+        <h2>Hi, I'm Chris Lee!</h2>
+        <h3>I'm a Full Stack Web Developer</h3>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,47 +1,51 @@
 import React, { useEffect } from 'react';
 
-function Nav(props) {
-const {
-    currentCategory,
-    setCurrentCategory
-} = props;
-
-<nav>
-<ul className="flex-row">
-
-<li className="mx-2">
-    <a href="#home" onClick={() => setCurrentCategory('#home')}>
-      Home
-    </a>
-  </li>
-
-  <li className="mx-2">
-    <a href="#about" onClick={() => setCurrentCategory('#about')}>
-      About me
-    </a>
-  </li>
-
-  <li className="mx-2">
-    <a href="#experience" onClick={() => setCurrentCategory('#experience')}>
-      My Experience
-    </a>
-  </li>
-
-  <li className="mx-2">
-    <a href="#projects" onClick={() => setCurrentCategory('#projects')}>
-      Projects
-    </a>
-  </li>
-
-  <li className="mx-2">
-    <a href="#contact" onClick={() => setCurrentCategory('#contact')}>
-      Contact Me
-    </a>
-  </li>
-
-</ul>
-</nav>
-
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#header"
+          onClick={() => handlePageChange('Header')}
+          // This is a conditional (ternary) operator that checks to see if the current page is "Header"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'Header' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#experience"
+          onClick={() => handlePageChange('Experience')}
+          // Check to see if the currentPage is `Experience`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Experience' ? 'nav-link active' : 'nav-link'}
+        >
+          Experience
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
 }
 
-export default Nav;
+export default NavTabs;
